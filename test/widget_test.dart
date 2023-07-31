abeff623bfd753d91b9ce2562d804bf38aa3337d
dev-Sara <mkmobasara.dev@gmail.com>
2023-08-01 00:37:58 +0600

    chained the structure

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:gurte_jaba/main.dart';
+import 'package:gurte_jaba/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
